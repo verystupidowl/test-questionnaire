b626e7ae9dc50b2aa7afcb1c80ced507467a36a0
Alexandra <amaklov2002@gmail.com>
2022-07-04 19:46:58 +0400

    init commit

---
+++ src/main/java/com/tgc/testQuestionnaire/TestQuestionnaireApplication.java
@@ -0,0 +1,13 @@
+package com.tgc.testQuestionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestQuestionnaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestQuestionnaireApplication.class, args);
+	}
+
+}
